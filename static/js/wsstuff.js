a73f34897b4189097d7a2465ad2287ceba33c649
Kevin Lin <stuy.kevinlin@gmail.com>
2020-01-24 14:11:51 -0500

    Changed HTTP to HTTPS for Heroku

--- static/js/wsstuff.js
+++ static/js/wsstuff.js
@@ -1,4 +1,4 @@
-var socket = io.connect('http://' + document.domain + ":" + location.port);
+var socket = io.connect('https://' + document.domain + ":" + location.port);
 var hiButton = document.getElementById('hiButton');
 var hiList = document.getElementById('hiList');
 
