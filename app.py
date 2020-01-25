b7adada2ebee8ebad457aff92ee4aa340251079b
Kevin Lin <stuy.kevinlin@gmail.com>
2020-01-25 18:22:31 -0500

    Drawing

--- app.py
+++ app.py
@@ -14,6 +14,18 @@ dbtools = Database.DBTools(app)
 
 print(dbtools.createDocID())
 
+lineStorage = { # Setup for temporary line storage may change depending on support for multiple pages
+    # documentID : {
+    #     'connectedUsers' : {request.sid},
+    #     'lines' : [
+    #         [x0, y0, x1, y1, lineWidth, 'rgba(r,g,b,a)']
+    #     ]
+    # }
+}
+
+connectedUsers = {
+    # request.sid : documentID
+}
 
 @app.route('/')
 def root():
@@ -121,20 +133,48 @@ def documentPage(documentID):
 def connectToDoc():
     print(f"A user has connected to a document page with sid {request.sid}")
 
+@socketio.on('disconnect', namespace = '/document')
+def disconnectFromDoc():
+    print(f"{request.sid} disconnected")
+    lineStorage[connectedUsers[request.sid]]['connectedUsers'].remove(request.sid)
+    print(lineStorage[connectedUsers[request.sid]]['connectedUsers'])
+    print(f"{len(lineStorage[connectedUsers[request.sid]]['connectedUsers'])} users remaining in {connectedUsers[request.sid]}")
+    if len(lineStorage[connectedUsers[request.sid]]['connectedUsers']) == 0:
+        lineStorage.pop(connectedUsers[request.sid])
+    connectedUsers.pop(request.sid)
+
 @socketio.on('joinDocument', namespace = '/document')
 def joinDocument(documentID):
     docIsPublic = True # Check if the document is public
+    successfulJoin = False
     if docIsPublic:
         join_room(documentID)
-        return
-    if "user" in session:
+        successfulJoin = True
+    elif "user" in session:
         userHasPermission = True # Check if the current user has access to the document
         if userHasPermission:
             join_room(documentID)
+            successfulJoin = True
         else:
             send('This user does not have permission to access the requested document.')
     else:
         send('This user is not logged in.')
+    if successfulJoin:
+        if documentID not in lineStorage: # Document not being viewed yet
+            lineStorage[documentID] = {
+                'connectedUsers' : {request.sid},
+                'lines' : []
+            }
+        else: # Document already being viewed
+            lineStorage[documentID]['connectedUsers'].add(request.sid)
+            emit('lines', lineStorage[documentID]['lines'])
+        connectedUsers[request.sid] = documentID
+
+@socketio.on('newLine', namespace = '/document')
+def newLine(line):
+    documentID = connectedUsers[request.sid]
+    lineStorage[documentID]['lines'].append(line)
+    emit('newLine', line, broadcast = True, include_self = False, room = documentID)
 
 @socketio.on('connect', namespace = '/socketioTest')
 def userConnect():
--- static/js/wsdocument.js
+++ static/js/wsdocument.js
@@ -1,12 +1,67 @@
 var protocol = (new URL(document.location)).protocol;
 var socket = io.connect(protocol + '//' + document.domain + ':' + location.port + '/document');
 
+var canvas = document.getElementById('mainCanvas');
+var ctx = canvas.getContext('2d');
+
+var lineWidth = 3;
+var color = 'rgba(0,0,0,1)';
+var prevX = 0;
+var prevY = 0;
+var isDrawing = false;
+
+var drawLine = function(x0, y0, x1, y1, inputWidth, inputColor, sendBack = true) {
+    ctx.lineWidth = inputWidth;
+    ctx.beginPath();
+    ctx.moveTo(x0, y0); //Offset x and y by vector
+    ctx.lineTo(x1, y1); //Draw line to center of the next circle
+    ctx.strokeStyle = inputColor
+    ctx.stroke();
+    if (sendBack) {
+        socket.emit('newLine', [x0, y0, x1, y1, inputWidth, inputColor]);
+    }
+}
+
 socket.on('connect', function() { //Executed upon opening the site
     var documentID = (new URL(document.location)).pathname.split('/').pop();
     socket.emit('joinDocument', documentID);
     console.log('Successfully connected');
 });
 
+socket.on('lines', function(lines) {
+    for (var i = 0; i < lines.length; i++) {
+        var currLine = lines[i];
+        drawLine(currLine[0], currLine[1], currLine[2], currLine[3], currLine[4], currLine[5], sendBack = false);
+    }
+});
+
+socket.on('newLine', function(line) {
+    drawLine(line[0], line[1], line[2], line[3], line[4], line[5], sendBack = false);
+});
+
 socket.on('message', function(msg) {
     console.log(msg);
+});
+
+canvas.addEventListener('mousedown', function(e) {
+    prevX = e.offsetX;
+    prevY = e.offsetY;
+    isDrawing = true;
+    drawLine(prevX, prevY, prevX, prevY, color);
+});
+
+canvas.addEventListener('mousemove', function(e) {
+    if (isDrawing) {
+        drawLine(prevX, prevY, e.offsetX, e.offsetY, color);
+        prevX = e.offsetX;
+        prevY = e.offsetY;
+    }
+});
+
+canvas.addEventListener('mouseout', function(e) {
+    isDrawing = false;
+});
+
+canvas.addEventListener('mouseup', function(e) {
+    isDrawing = false;
 });
